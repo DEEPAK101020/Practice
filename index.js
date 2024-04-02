8c6bafa7347c7617d97aa939d5fb9933e7d793f1
DEEPAK101020 <deepakkumarktr1998@gmail.com>
2024-04-02 13:41:54 +0530

    practice

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // let n=ans.length;
 // console.log( ans[n-1].length);
 
-let arr=[1,2,3,4,5];
+// let arr=[1,2,3,4,5];
 
 // let res=arr.map((ele)=>{
 //     return ele*2;
@@ -35,3 +35,21 @@ let arr=[1,2,3,4,5];
 
 // console.log(c);
 // var c=10;//undefined
+
+
+
+// var x=10;
+// var fun=function(){
+// console.log(x);
+// var x=20;
+// }
+
+// fun();//undefined
+
+//rest spread
+function sum(...nums){ //...nums is rest
+console.log(nums)
+
+}
+var arr=[5,6];
+sum(...arr)//her it is called spread 
